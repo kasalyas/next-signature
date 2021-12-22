b64c90b2d2b82252f9b072b508433ce3a57519bc
Kas Alyas <kas.alyas@kinandcarta.com>
2021-12-22 09:22:37 +0000

    moved provider out to _app

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,17 +1,14 @@
-import SignatureState from "../context";
 import Footer from "../Footer";
 import Header from "../Header";
 import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <SignatureState>
-      <div id={styles.app}>
-        <Header />
-        <main className={styles.grow}>{children}</main>
-        <Footer />
-      </div>
-    </SignatureState>
+    <div id={styles.app}>
+      <Header />
+      <main className={styles.grow}>{children}</main>
+      <Footer />
+    </div>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
+import SignatureState from "../components/context";
 import "../main.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SignatureState>
+      <Component {...pageProps} />
+    </SignatureState>
+  );
 }
