b6b44a7bc6655f6fd89602f510b818bc16f3428a
Kas Alyas <kas.alyas@kinandcarta.com>
2022-01-10 16:55:51 +0000

    removed path update

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === "production";
-
-console.log(`what is production: ${isProd}`);
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? "./" : "",
-};
